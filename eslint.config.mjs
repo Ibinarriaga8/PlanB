a95dfe31c3837383bcfcdf0eccd5597f8b6552de
Ibinarriaga8 <202206789@alu.comillas.edu>
2026-06-18 10:36:59 +0200

    fix: ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,15 @@ export default [
     plugins: {
       "@next/next": nextPlugin,
     },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs["core-web-vitals"].rules,
